d16375dbd0b93d2a81e29e521dd180eecd123e42
ristoxxx <lahteri@gmail.com>
2023-12-18 23:03:08 +0200

    Update Memeber.java

--- src/main/java/com/ristoxxx/restservice/model/Memeber.java
+++ src/main/java/com/ristoxxx/restservice/model/Memeber.java
@@ -1,7 +1,5 @@
 package com.ristoxxx.restservice.model;
 
-import java.util.Objects;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
