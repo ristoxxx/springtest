f72b212a6abcb72ecb07961bb704c65f517a4669
ristoxxx <lahteri@gmail.com>
2023-12-18 23:01:53 +0200

    Update Memeber.java

---
+++ src/main/java/com/ristoxxx/restservice/model/Memeber.java
@@ -0,0 +1,32 @@
+package com.ristoxxx.restservice.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+
+@Entity
+public class Memeber {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String memberName;
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+
+
+    
+}
