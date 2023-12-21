7d5940eb952a654ec859972344e86dd924f62343
ristoxxx <lahteri@gmail.com>
2023-12-21 20:39:40 +0200

    Update Genre.java

--- src/main/java/com/ristoxxx/restservice/model/Genre.java
+++ src/main/java/com/ristoxxx/restservice/model/Genre.java
@@ -1,9 +1,27 @@
 package com.ristoxxx.restservice.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Genre {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+
+    private String genre;
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
    
     
 }
